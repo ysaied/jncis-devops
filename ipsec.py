810c00dde25113fd3c47ee31405914debbadf060
Yasser Saied <ysaied80@yahoo.com>
2020-10-15 15:32:40 +0400

    Update ipsec.py

--- ipsec.py
+++ ipsec.py
@@ -1,11 +1,12 @@
 #! /usr/bin/python
 from jinja2 import Environment, FileSystemLoader
-from yaml import load
+import yaml
 
 env = Environment(loader=FileSystemLoader("jinja2"))
 ipsec_jinja2 = env.get_template("ipsec.j2")
 
 with open("vars/ipsec.yml") as f:
-    data = load(f, Loader=FullLoader)
+    data = yaml.load(f, Loader=yaml.FullLoader)
 
-print (ipsec_jinja2.render(data))
+print (data)
+#print (ipsec_jinja2.render(data))
