9ee604061372d927e8735237cbdf19d5f1e7d626
Yasser Saied <ysaied80@yahoo.com>
2020-10-15 15:47:39 +0400

    Update ipsec.py

--- ipsec.py
+++ ipsec.py
@@ -8,5 +8,9 @@ ipsec_jinja2 = env.get_template("ipsec.j2")
 with open("./vars/ipsec.yml") as f:
     data = yaml.load(f, Loader=yaml.FullLoader)
 
-print (data)
-print (ipsec_jinja2.render(data))
+#print (data)
+#print (ipsec_jinja2.render(data))
+
+with open("./j2-results/junos.txt", "w") as config:
+    print >> config, (ipsec_jinja2.render(data))
+    config.close()
