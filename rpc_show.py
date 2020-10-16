c7e940b20ba21d22eb859bd04fb33dc241e9ab0f
Yasser Saied <ysaied80@yahoo.com>
2020-10-16 16:49:40 +0400

    Update rpc_show.py

--- rpc_show.py
+++ rpc_show.py
@@ -11,6 +11,7 @@ out_fname = "rpc_show-" + datetime.now().strftime("%d%h%Y") + ".txt"
 with open("./j2-results/" + out_fname, "w") as results:
 # create Device instance to connect and get device info
     with Device(host="192.0.0.41", user="ysaied") as jnpr_node:
-        result_1 = etree.tostring(jnpr_node.rpc.get_system_uptime_information(), encoding='unicode', pretty_print=True)
-        result_2 =  result_1.findtext("system-uptime-information/current-time/date-time")
+        result = jnpr_node.rpc.get_system_uptime_information()
+        result_1 = etree.tostring(result, encoding='unicode', pretty_print=True)
+        result_2 =  result.findtext("system-uptime-information/current-time/date-time")
         print >> results, (result_2)
