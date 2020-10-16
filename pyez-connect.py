614c3f123fbafa4738bfe3d07563756bffad360f
Yasser Saied <ysaied80@yahoo.com>
2020-10-16 15:58:56 +0400

    Update pyez-connect.py

--- pyez-connect.py
+++ pyez-connect.py
@@ -2,5 +2,8 @@
 
 from jnpr.junos import Device
 
-with Device(host="192.0.0.41", user="ysaied") as jnpr_node:
-    print(jnpr_node.facts)
+
+out_fname = "pyez_connect-" + datetime.now().strftime("%d%h%Y") + ".txt"
+with open("./j2-results/" + out_fname, "w") as results:
+    with Device(host="192.0.0.41", user="ysaied") as jnpr_node:
+        print >> results, (jnpr_node.facts)
