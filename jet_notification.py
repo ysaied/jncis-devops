c717e7b1c5958778117547484b6b17369e96207f
Yasser Saied <ysaied80@yahoo.com>
2020-11-15 22:04:42 +0400

    Update jet_notification.py

--- jet_notification.py
+++ jet_notification.py
@@ -5,7 +5,7 @@ def mqtt_callback(client, user_data, message):
     print("Message with topic {} received: {}".format(message.topic, str(message.payload.decode("utf-8"))))
 
 device = "3.126.37.30"
-topic1 = "/junos/events/#"
+topic1 = "/junos/events/kernel/route/#"
 
 if __name__ == "__main__":
     print "connecting to MQTT brocker"
