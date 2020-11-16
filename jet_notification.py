9d5fe416df44ca9b6ee0c1ee0915065f07b06895
Yasser Saied <ysaied80@yahoo.com>
2020-11-16 11:52:15 +0400

    Update jet_notification.py

--- jet_notification.py
+++ jet_notification.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     
     client.subscribe(topic1)
 
-    raw_input("Press Enter to stop")
+    raw_input("Press Enter to stop \n")
     client.loop_stop()
     client.disconnect()
