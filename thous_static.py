edc98ebfedf361c60bc6192cf40aa16e145caba8
Yasser Saied <ysaied80@yahoo.com>
2020-11-30 19:51:07 +0400

    Update thous_static.py

--- thous_static.py
+++ thous_static.py
@@ -2,9 +2,11 @@
 
 a = 0
 
+file_output = open(/var/tmp/test, "w")
 while a <= 255 :
     b = 0
     while b <= 255 :
-        print ("set routing-options static route 192.1.{}.{}/32 reject".format(a,b))
+        print >> file_output,("set routing-options static route 192.1.{}.{}/32 reject".format(a,b))
         b += 1
     a += 1
+file_output.close()
