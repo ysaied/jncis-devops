c84d555cf881da6d3a4c167c0575767be8dbfaf9
Yasser Saied <ysaied80@yahoo.com>
2020-11-30 20:09:37 +0400

    Update thous_static.py

--- thous_static.py
+++ thous_static.py
@@ -13,6 +13,6 @@ a = 0
 
 file_output = open("/var/tmp/l3vpn", "w")
 while a <= 8000 :
-    print >> file_output,("set routing-instances l3vpn-{} instance-type vrf".format("%02d"%a))
+    print >> file_output,("set routing-instances l3vpn-{} instance-type vrf".format("%04d"%a))
     a +=1
 file_output.close()
