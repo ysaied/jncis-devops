724f64d5ee21835008b01612861e67d64dd43c0a
Yasser Saied <ysaied80@yahoo.com>
2020-11-30 20:03:48 +0400

    Update thous_static.py

--- thous_static.py
+++ thous_static.py
@@ -2,11 +2,17 @@
 
 a = 0
 
-file_output = open("/var/tmp/test", "w")
-while a <= 255 :
-    b = 0
-    while b <= 255 :
-        print >> file_output,("set routing-options static route 192.1.{}.{}/32 reject".format(a,b))
-        b += 1
-    a += 1
+#file_output = open("/var/tmp/test", "w")
+#while a <= 255 :
+#    b = 0
+#    while b <= 255 :
+#        print >> file_output,("set routing-options static route 192.1.{}.{}/32 reject".format(a,b))
+#        b += 1
+#    a += 1
+#file_output.close()
+
+file_output = open("/var/tmp/l3vpn", "w")
+while a <= 1000 :
+    print >> file_output,("set routing-instances l3vpn-{} instance-type vrf".format(a))
+    a +=1
 file_output.close()
