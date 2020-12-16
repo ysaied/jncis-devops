b5d8c9104a8a263e74ed8ed2da6d85e1c1798999
Yasser Saied <ysaied80@yahoo.com>
2020-12-16 11:54:35 +0400

    Update thous_static.py

--- thous_static.py
+++ thous_static.py
@@ -2,15 +2,6 @@
 
 a = 0
 
-#file_output = open("/var/tmp/test", "w")
-#while a <= 255 :
-#    b = 0
-#    while b <= 255 :
-#        print >> file_output,("set routing-options static route 192.1.{}.{}/32 reject".format(a,b))
-#        b += 1
-#    a += 1
-#file_output.close()
-
 config = """
 set routing-instances l3vpn-{0} instance-type vrf"
 set routing-instances l3vpn-{0} route-distinguisher 192.1.0.0:{1}"
