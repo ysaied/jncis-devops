11a11cfe4a0c31390b0d54caf9dcb6c19c8f30f7
Yasser Saied <ysaied80@yahoo.com>
2021-02-02 16:25:50 +0400

    Update qfx-sp-to-ent-sample.py

--- qfx-sp-to-ent-sample.py
+++ qfx-sp-to-ent-sample.py
@@ -39,7 +39,6 @@ for leaf,oob in Leafs.items():
   ifd_set=set()
   vlan_id_set=set()
   for vlan in vlan_list:
-    #ifl_xpath = "vlans/vlan[name='"+vlan+"']/interface"
     ifl_xpath = "vlans/vlan[name='{}']/interface".format(vlan)
     ifl_list = config.findall(ifl_xpath)
     for ifl in ifl_list:
@@ -48,9 +47,14 @@ for leaf,oob in Leafs.items():
       ifd_set.add(ifd_name)
       vlan_id = vlan.split("-")[1]
       vlan_id_set.add(vlan_id)
-      print("delete vlans {} interface {}".format(vlan,ifl_name), file=open_file)
-      print("delete interfaces {}".format(ifl_name), file=open_file)
-      print("set interfaces {}.0 family ethernet-switching vlan members {}".format(ifd_name,vlan_id), file=open_file)
+      print("""
+      delete vlans {0} interface {2}
+      delete interfaces {2}
+      set interfaces {1}.0 family ethernet-switching vlan members {3}
+      """.format(vlan,ifd_name,ifl_name,vlan_id),file=open_file)
+      #print("delete vlans {} interface {}".format(vlan,ifl_name), file=open_file)
+      #print("delete interfaces {}".format(ifl_name), file=open_file)
+      #print("set interfaces {}.0 family ethernet-switching vlan members {}".format(ifd_name,vlan_id), file=open_file)
   
   vlan_id_list=list(vlan_id_set)
   vlan_id_list.sort()
