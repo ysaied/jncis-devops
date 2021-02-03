c588768230aa665e1d95b581509b76f4e62e1c77
Yasser Saied <ysaied80@yahoo.com>
2021-02-03 15:24:57 +0400

    a

--- evpn-sp-to-ent.py
+++ evpn-sp-to-ent.py
@@ -1,71 +1,83 @@
-import subprocess
-import sys
+#!/usr/bin/python3
 
-#################################################################################################################################
-# Accetta in input un file di testo che contiene la configurazione delle leaf
-# in fornato Service Provider e fornisce in uscita i comandi per trasformarla in formato Enterprise
-#
-# La configurazione in ingresso e' ottenuta dal comando "show configuration | display set | no-more"
-#################################################################################################################################
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from jnpr.junos.utils.scp import SCP
+from lxml import etree
+from datetime import datetime
+import re
 
-INVENTORY = []
+today = datetime.now().strftime("%d-%h-%Y")
+username = "juniper"
 
-# tzRoma=timezone(timedelta(hours=1), name='tzRoma')
-# print("{:s} - {:s}".format(datetime.now().astimezone(tz=tzRoma).isoformat(), 'Starting ....'))
-# print("{:s} - {:s}".format(datetime.now().astimezone(tz=tzRoma).isoformat(), 'Getting management address from VMM'))
-#################################################################################################################################
-#
-# Create list of Vlan with associated VNI
-#
-#################################################################################################################################
+Leafs = {
+  "Leaf3" : "192.0.0.61",
+  }
 
-for LINE in open(sys.argv[1]):
-  if 'vxlan vni' in LINE:
-    NOMEVLAN = LINE.split()[2]
-    INVENTORY.append( { 'nomevlan':NOMEVLAN, 'listaifl':[] } )
+for leaf,oob in Leafs.items():
+  dev = Device(host=oob, user=username, gather_facts="false")
+  dev.open()
+  config = dev.rpc.get_config()
+  dev.close()
+  #etree.dump(config)
+  vlans = config.findall('vlans/vlan')
 
-#################################################################################################################################
-#
-# Attach ifl to Vlans
-#
-#################################################################################################################################
+  vlan_list = list()
+  for vlan in vlans:
+    vlan_name = vlan.find('name').text
+    vlan_list.append(vlan_name)
+  vlan_list.sort()
 
-for LINE in open(sys.argv[1]):
-  if ('set vlans' in LINE) and ('interface' in LINE):
-    NOMEVLAN = LINE.split()[2]
-    if NOMEVLAN in [nomevlan1['nomevlan'] for nomevlan1 in INVENTORY]:
-      NOMEIFL = LINE.split()[4]
-      [VLAN1['listaifl'].append(NOMEIFL) for VLAN1 in INVENTORY if VLAN1.get('nomevlan', 0) == NOMEVLAN]
+  file_name = "/var/tmp/{}_sp-to-ent-migration_{}.txt".format(leaf,today)
+  open_file = open(file_name, "w")
 
-# print(str(INVENTORY).replace("'", '"'))
-# print(str(INVENTORY))
-# print()
+  ifd_set=set()
+  vlan_id_set=set()
+  for vlan in vlan_list:
+    ifl_xpath = "vlans/vlan[name='{}']/interface".format(vlan)
+    ifl_list = config.findall(ifl_xpath)
+    for ifl in ifl_list:
+      ifl_name = ifl.find("name").text
+      ifd_name = ifl_name.split(".")[0]
+      ifd_set.add(ifd_name)
+      vlan_id = vlan.split("-")[1]
+      vlan_id_set.add(vlan_id)
+      print("delete vlans {} interface {}".format(vlan,ifl_name), file=open_file)
+      print("delete interfaces {}".format(ifl_name), file=open_file)
+      print("set interfaces {}.0 family ethernet-switching vlan members {}".format(ifd_name,vlan_id), file=open_file)
+  
+  vlan_id_list=list(vlan_id_set)
+  vlan_id_list.sort()
+  for vlan_id in vlan_id_list:
+    print("set vlans VLAN-{0} vlan-id {0}".format(vlan_id), file=open_file) 
 
-#################################################################################################################################
-#
-# Delete ifls from Vlans and ifd
-#
-#################################################################################################################################
+  ifd_list=list(ifd_set)
+  ifd_list.sort()  
+  for ifd in ifd_list:
+    print("set interfaces {}.0 family ethernet-switching interface-mode trunk".format(ifd), file=open_file)
+    print("set interfaces {}.0 family ethernet-switching storm-control storm-control-standard".format(ifd), file=open_file)
+  
+  open_file.close()
+  
+  with SCP(dev, progress=True) as scp:
+    scp.put(file_name, remote_path="/var/home/"+username)
 
-for VLAN1 in INVENTORY:
-  UNIT=VLAN1['nomevlan'].replace("VLAN-", "")
-  for IFL1 in VLAN1['listaifl']:
-    IFD = IFL1.split(".")[0]
-    print("delete vlans {:s} interface {:s}".format(VLAN1['nomevlan'], IFL1))
-    print("delete interfaces {:s} unit {:s}".format(IFD, UNIT))
 
-#################################################################################################################################
-#
-# Recreate ifd and Vlans
-#
-#################################################################################################################################
+def xml_conf(hostname, oob):
+  with Device(host=oob, user=username, gather_facts="false") as dev:
+    config = dev.rpc.get_config()
+    return config
 
-for VLAN1 in INVENTORY:
-  UNIT=VLAN1['nomevlan'].replace("VLAN-", "")
-  print("set vlans {:s} vlan-id {:s}".format(VLAN1['nomevlan'], UNIT))
-  for IFL1 in VLAN1['listaifl']:
-    IFD = IFL1.split(".")[0]
-    print("set interfaces {:s} unit 0 family ethernet-switching storm-control storm-control-standard".format(IFD))
-    print("set interfaces {:s} unit 0 family ethernet-switching interface-mode trunk vlan members {:s}".format(IFD, VLAN1['nomevlan']))
+def vlan_names(config):
+  vlan_xpath = config.findall('vlans/vlan')
+  vlan_list = list()
+  for vlan in vlan_xpath:
+    vlan_name = vlan.find('name').text
+    vlan_list.append(vlan_name)
+  vlan_list.sort()
+  if vlan_list:
+    return vlan_list
+  else:
+    return None
 
 
